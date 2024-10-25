51891f746cf94d3ddec0d3cee57838cf35298738
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-10-25 14:16:27 +0300

    button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,19 +5,19 @@ const App = () => {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
-    <div className="pt-[4.75rem] lg:[5.25rem] overflow-hidden">
+      <div className="pt-[4.75rem] lg:[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          something
+        </Button>
+      </div>
 
-    </div>
-    <Button className="mt-10" href="#login">
-      something
-    </Button>
-    <ButtonGradient />
+      <ButtonGradient />
     </>
-    
+
   )
 }
 
