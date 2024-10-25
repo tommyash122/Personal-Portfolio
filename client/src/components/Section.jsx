436222c62c3cb057b33291e1774accdb4cab2c5a
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-10-25 18:10:45 +0300

    start working on section

---
+++ client/src/components/Section.jsx
@@ -0,0 +1,16 @@
+const Section = ({ 
+    className,
+    id,
+    crosses,
+    crossesOffset,
+    customPaddings,
+    children
+}) => {
+    return (
+        <div className={`relative ${customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl:py-40' : ''} ${className || ''}`}`}>
+
+        </div>
+    )
+}
+
+export default Section
